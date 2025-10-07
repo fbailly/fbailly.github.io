da3b3f1994700b3a2963bbe0f0754ccd0a5469d2
fbailly <francois.bailly@inria.fr>
2025-10-07 11:59:02 +0200

    Update 2025 journal pubs

--- build.py
+++ build.py
@@ -15,9 +15,9 @@ def get_personal_data():
                 </p>
                 <p>
                     <span style="font-weight: bold;">On-going projects:</span>
+                    In 2025, I started an <a href=https://team.inria.fr/acer/ target="_blank">associated team</a> with Université de Montréal.
                     In 2023, I have been awarded an ANR JCJC grant for the B-IRD project, on fast and reliable biomechanical methods dedicated to assistive technologies.
                     Since 2023 I am part of the european EIC Pathfinder AI-HAND project led by INRIA on implanted neuroprosthese for hand movement restoration.
-                    Since 2022 I am part of the FLAG-ERA JCT 2021 <a href="https://rescue-graph.com/" target="_blank">Rescuegraph</a> project led by <a href="https://www.uab.cat/" target="_blank">UAB</a> on functional stimulation system using graphene-based nerve electrodes.
                 </p>
                 <p>For any inquiries, feel free to reach out to me via mail!</p>
                 <p>
